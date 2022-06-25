dcbdb76a149defb77aeba9381d00f641d8692a29
unknown <416212316@qq.com>
2022-06-25 22:25:12 +0800

    fix

--- src/core/vdom/patch.js
+++ src/core/vdom/patch.js
@@ -533,6 +533,7 @@ export function createPatchFunction (backend) {
     // note we only do this if the vnode is cloned -
     // if the new node is not cloned it means the render functions have been
     // reset by the hot-reload-api and we need to do a proper re-render.
+    // 静态节点处理
     if (isTrue(vnode.isStatic) &&
       isTrue(oldVnode.isStatic) &&
       vnode.key === oldVnode.key &&
@@ -558,21 +559,25 @@ export function createPatchFunction (backend) {
       if (isDef(i = data.hook) && isDef(i = i.update)) i(oldVnode, vnode)
     }
     // 根据双方类型的几种情况分别处理
-    if (isUndef(vnode.text)) {
+    if (isUndef(vnode.text)) {// 新节点没有文本
       if (isDef(oldCh) && isDef(ch)) {
+        // 双方都有子元素,就进行重排，diff
         if (oldCh !== ch) updateChildren(elm, oldCh, ch, insertedVnodeQueue, removeOnly)
       } else if (isDef(ch)) {
+        // 新节点有孩子, 老的没有，新增创建
         if (process.env.NODE_ENV !== 'production') {
           checkDuplicateKeys(ch)
         }
         if (isDef(oldVnode.text)) nodeOps.setTextContent(elm, '')
         addVnodes(elm, null, ch, 0, ch.length - 1, insertedVnodeQueue)
       } else if (isDef(oldCh)) {
+        // 新节点没有孩子，老的有的，则删除
         removeVnodes(oldCh, 0, oldCh.length - 1)
       } else if (isDef(oldVnode.text)) {
         nodeOps.setTextContent(elm, '')
       }
     } else if (oldVnode.text !== vnode.text) {
+      // 新老节点都是文本节点，则进行文本更新
       nodeOps.setTextContent(elm, vnode.text)
     }
     if (isDef(data)) {
